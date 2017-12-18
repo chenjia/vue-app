264f395d99655a892f6b7cc6dd06144c82fdeefb
chenjia <chenjia@n22.com.cn>
2017-12-18 15:10:52 +0800

    cdscds

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,12 +2,14 @@ import cache from './cache'
 import format from './format'
 import http from './http'
 import ui from './ui'
+import version from './version'
 
 const utils = {
   cache: cache,
   format: format,
   http: http,
-  ui: ui
+  ui: ui,
+  version: version
 }
 
 export default utils
