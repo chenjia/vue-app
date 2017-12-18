b3a646bb2507432a3af3edd74e486d30f25e625e
chenjia <chenjia@n22.com.cn>
2017-12-18 16:42:09 +0800

    update

--- src/utils/version.js
+++ src/utils/version.js
@@ -3,7 +3,14 @@ import store from '../vuex/store'
 const version = {
 	ready(){
 		document.addEventListener('chcp_updateIsReadyToInstall', () => {
-			store.commit('TOGGLE_POPUP', {visible: true, text: '正在更新版本...'})
+			store.commit('TOGGLE_POPUP', {visible: true, text: '正在更新版本'})
+		}, false);
+
+		document.addEventListener('chcp_nothingToUpdate', () => {
+			store.commit('TOGGLE_POPUP', {visible: true, text: '已经是最新版本'})
+			setTimeout(()=>{
+				store.commit('TOGGLE_POPUP', {visible: false, text: ''})
+			},1000)
 		}, false);
 
 		document.addEventListener('chcp_updateInstalled', () => {
@@ -19,6 +26,11 @@ const version = {
 	},
 	update(){
 		console.log('update')
+		chcp.fetchUpdate(() => {
+			
+		}, {
+			'config-file': 'https://chenjia.github.io/vue-app/demo/chcp.json'
+		})
 	}
 };
 
