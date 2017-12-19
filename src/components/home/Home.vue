71b1f06044c4625be258a1596470d31c41ece3a2
chenjia <chenjia@n22.com.cn>
2017-12-19 17:04:37 +0800

    fsfd

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lxt-page">
-    <mt-header title="系统首页">
+    <mt-header title="首页">
       <mt-button slot="left" class="fa fa-fw fa-bars"></mt-button>
       <mt-button slot="right" @click="go('/page/login')" class="fa fa-fw fa-lock"></mt-button>
     </mt-header>
