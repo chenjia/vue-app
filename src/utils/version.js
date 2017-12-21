605204fe097745c7cc16d0ae38947fdc1a91dd47
chenjia <chenjia@n22.com.cn>
2017-12-21 13:06:53 +0800

    fds

--- src/utils/version.js
+++ src/utils/version.js
@@ -24,6 +24,7 @@ const version = {
 	update(){
 		store.commit('TOGGLE_POPUP', {visible: true, text: '正在检测新版本'})
 		chcp.fetchUpdate((error, data) => {
+			alert(JSON.stringify(error)+' '+JSON.stringify(data))
 			if(data.description){
 				alert(data.description)	
 			}
