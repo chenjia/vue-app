ce9b7d8c6257e5948d44c68a2d93ba6a0bcef05a
chenjia <chenjia@n22.com.cn>
2018-02-02 17:11:59 +0800

    update

--- src/components/form/Form.vue
+++ src/components/form/Form.vue
@@ -31,8 +31,8 @@
       <mt-cell class="mint-field">
         <span slot="title">电量 {{light}}%</span>
         <mt-range v-model="light" :min="0" :max="100" style="width:100%;">
-          <div slot="start"><i class="fa fa-battery-empty"></i>&nbsp;</div>
-          <div slot="end">&nbsp;<i class="fa fa-battery-4" style="color:#26a2ff;"></i></div>
+          <div slot="start" style="align-items:center;"><i class="fa fa-battery-empty"></i>&nbsp;</div>
+          <div slot="end" style="align-items:center;">&nbsp;<i class="fa fa-battery-4" style="color:#26a2ff;"></i></div>
         </mt-range>
       </mt-cell>
       <mt-radio title="单选框列表" v-model="radioValue" :options="['选项A', '选项B', '选项C']"></mt-radio>
