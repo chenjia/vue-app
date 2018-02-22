98ac547e5c5464cc5ffebaf85789b7cda2a75262
chenjia <chenjia@n22.com.cn>
2018-02-22 14:37:18 +0800

    update

--- src/components/common/Timeline.vue
+++ src/components/common/Timeline.vue
@@ -26,7 +26,7 @@ export default {
   data() {
     return {
       color:[
-      	'#ff7f50', '#87cefa', '#da70d6', '#32cd32', '#6495ed', 
+      	'#87cefa', '#ff7f50', '#da70d6', '#32cd32', '#6495ed', 
         '#ff69b4', '#ba55d3', '#cd5c5c', '#ffa500', '#40e0d0', 
         '#1e90ff', '#ff6347', '#7b68ee', '#00fa9a', '#ffd700', 
         '#6b8e23', '#ff00ff', '#3cb371', '#b8860b', '#30e0e0'
