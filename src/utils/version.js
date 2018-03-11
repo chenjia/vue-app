83ac3fb75c8e56706b4d7cd2bdc2468cea1ca4ac
chenjia <chenjia@n22.com.cn>
2018-03-11 23:30:32 +0800

    aaa

--- src/utils/version.js
+++ src/utils/version.js
@@ -9,6 +9,11 @@ const version = {
 			callback(err, versionInfo)
 		})
 	},
+	updateVersionInfo(){
+		version.getVersionInfo((err, versionInfo) => {
+			store.commit('UPDATE_VERSION', versionInfo.currentWebVersion)
+		})
+	},
 	fetchUpdate(){
 		if(!window.chcp){return}
 		store.commit('TOGGLE_POPUP', {visible: true, text: '正在检测新版本'})
@@ -16,7 +21,7 @@ const version = {
 			chcp.fetchUpdate((error, data) => {
 				store.commit('TOGGLE_POPUP', {visible: true, text: '正在下载新版本...'})
 				let config = JSON.parse(data.config)
-				if(config.native_version != window.Config.native_version){
+				if(config.native_version != window.native_version){
 					store.commit('TOGGLE_POPUP', {visible: true, text: '当前版本过低，请安装最新版本'})
 					alert('当前版本过低，请安装最新版本！')
 					window.open(Config.appUrl)
@@ -31,7 +36,7 @@ const version = {
 					}else{
 						if(config.release != versionInfo.currentWebVersion){
 							MessageBox('版本提示（'+config.release+'）', config.description).then(action => {
-							  this.installUpdate(config, versionInfo)
+							  this.installUpdate(config)
 							})
 							setTimeout(()=>{
 								document.querySelector('.mint-msgbox-message').style.textAlign = 'left'
@@ -44,14 +49,13 @@ const version = {
 			})
 		})
 	},
-	installUpdate(config, versionInfo){
+	installUpdate(config){
 		store.commit('TOGGLE_POPUP', {visible: true, text: '正在安装新版本'})
 		chcp.installUpdate(error => {
 			if (error) {
 				store.commit('TOGGLE_POPUP', {visible: true, text: '更新包安装失败', duration: 3000})
 				alert(JSON.stringify(error))
 	    } else {
-	    	store.commit('UPDATE_VERSION', versionInfo.currentWebVersion)
 	      store.commit('TOGGLE_POPUP', {visible: true, text: '已经更新为最新版本', duration: 1000})
 	    }
 		})
@@ -59,7 +63,7 @@ const version = {
 }
 
 document.addEventListener("deviceready", () => {
-	
+	version.updateVersionInfo()
 }, false)
 
 export default version
