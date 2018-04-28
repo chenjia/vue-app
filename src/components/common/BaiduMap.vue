07a32146d9821a7f63f78c8ecef6ee3a8a680eff
chenjia <chenjia@n22.com.cn>
2018-04-28 14:29:15 +0800

    aa

--- src/components/common/BaiduMap.vue
+++ src/components/common/BaiduMap.vue
@@ -22,8 +22,10 @@
       window.mapCallback = BMap => {
         const map = new BMap.Map("mapBox")
         map.centerAndZoom(new BMap.Point(121.506396,31.24495),15)
+        alert('正则获取GPS坐标...')
         navigator.geolocation.getCurrentPosition(function(position){
           console.log(position)
+          alert('获取GPS坐标成功')
           const currentLat = position.coords.latitude
           const currentLon = position.coords.longitude
           const gpsPoint = new BMap.Point(currentLon, currentLat)
@@ -42,7 +44,7 @@
           })
         }, function(err){
           console.log(err)
-          alert(err.message)
+          alert(JSON.stringify(err))
         })
       }
     }
