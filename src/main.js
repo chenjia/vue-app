727a41b80a0fa85b0bbe92c31fdc928d0c7e7d17
chenjia <chenjia@n22.com.cn>
2018-06-13 16:29:15 +0800

    aa

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 window.Config = {
-  appVersion:'1.0.19',
+  appVersion:'1.0.20',
   nativeVersion:'1.0',
   server: 'http://localhost:8001/api',
   appDownloadUrl:'https://chenjia.github.io/vue-app/demo/index.html',
