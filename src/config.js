5e46bf55a42191fcd27c18214b988e4592f18245
chenjia <251746034@qq.com>
2019-05-17 19:34:36 +0800

    aa

--- src/config.js
+++ src/config.js
@@ -7,5 +7,5 @@ window.Config = {
   chcpUrl:'https://chenjia.github.io/vue-app/public/chcp.json',
   key:'dzg5czJyejd5NjFkN3g3MjNjc2QwamN3OXUxODFjb280YWE4djhvYmV1NW9lemUxMzFjdGFiYTZ0OXo5dXhkMWNrYzRvZHZ3NjcyMzZkdWU=',
   random:new Date().getTime(),
-  preload: 1111
+  preload: 1000
 }
