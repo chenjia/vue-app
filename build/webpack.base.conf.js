722b9fee988211b8965883858cb244fa08b1df8b
chenjia <251746034@qq.com>
2019-05-17 19:44:29 +0800

    aa

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -34,7 +34,9 @@ module.exports = {
       include: [
         resolve('src'),
         resolve('test'),
+        resolve('/node_modules/pinyin'),
         resolve('/node_modules/_pinyin@2.8.3@pinyin'),
+        resolve('/node_modules/signature_pad'),
         resolve('/node_modules/_signature_pad@3.0.0-beta.3@signature_pad')
       ]
     }, {
