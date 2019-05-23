fb2b833df623f36b26109b6ea44467ac54b55414
chenjia <251746034@qq.com>
2019-05-23 18:30:31 +0800

    aa

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,4 +1,4 @@
-import {encryptByDES,decryptByDES,encryptKey,decryptKey} from './security'
+import {encryptByAES,decryptByAES,encryptKey,decryptKey} from './security'
 
 const cache = {
 	set(key, value, seconds = 60*60*24) {
@@ -6,13 +6,13 @@ const cache = {
 			value = JSON.stringify(value)
 		}
 		value = JSON.stringify({value:value, expired: new Date().getTime()+seconds*1000})
-		localStorage.setItem(key, encryptByDES(value, decryptKey(window.Config.key)))
+		localStorage.setItem(key, encryptByAES(value, decryptKey(window.Config.key)))
 	},
 	get(key) {
 		let value = localStorage.getItem(key)
 
 		if(value){
-			value = eval('(' + decryptByDES(value, decryptKey(window.Config.key)) + ')')
+			value = eval('(' + decryptByAES(value, decryptKey(window.Config.key)) + ')')
 			if(value.expired > new Date().getTime()){
 				value = value.value
 				if (value.substr(0, 1) == '{' || value.substr(0, 1) == '[') {
