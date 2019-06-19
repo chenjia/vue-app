4099d948bdc03bd367ab6a99d82368e9cad0f3ee
chenjia <251746034@qq.com>
2019-06-19 17:41:55 +0800

    aa

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ window.Config = {
   appVersion:'1.0.7',
   nativeVersion:'1.0',
   // server: 'http://localhost:8201/api',
-  server: 'http://47.100.119.102/api',
+  server: 'http'+(location.href.indexOf('https')==0?'s':'')+'://47.100.119.102/api',
   appDownloadUrl:'http://47.100.119.102/vue-app',
   chcpUrl:'http://47.100.119.102/vue-app/chcp.json',
   key:'dzg5czJyejd5NjFkN3g3MjNjc2QwamN3OXUxODFjb280YWE4djhvYmV1NW9lemUxMzFjdGFiYTZ0OXo5dXhkMWNrYzRvZHZ3NjcyMzZkdWU=',
